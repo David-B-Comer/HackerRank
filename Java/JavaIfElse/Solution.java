a46bfe34f92a7631fc98832520eec6eb814a796c
David-B-Comer <davec2233@gmail.com>
2020-06-09 18:43:09 -0400

    added solution

--- Java/JavaIfElse/Solution.java
+++ Java/JavaIfElse/Solution.java
@@ -48,5 +48,29 @@ package JavaIfElse;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String result="";
+        if(n%2==1 || ( (n%2==0) && (n>=6 && n <= 20))) {
+            result = "Weird";
+        }
+        else {
+            result = "Not Weird";
+        }
+        System.out.println(result);
+
+    }
 }
