c4f0ce455e8ae1a698361438d697a8a8ba877c51
David-B-Comer <davec2233@gmail.com>
2020-06-12 21:56:12 -0400

    added solution

--- Java/JavaStaticInitializerBlock/Solution.java
+++ Java/JavaStaticInitializerBlock/Solution.java
@@ -1,5 +1,7 @@
 package JavaStaticInitializerBlock;
 
+import java.util.Scanner;
+
 /**
 
  Static initialization blocks are executed when the class is loaded, and you can initialize static variables in those blocks.
@@ -38,4 +40,17 @@ package JavaStaticInitializerBlock;
  **/
 
 public class Solution {
+    static boolean flag = true;
+    static int B;
+    static int H;
+    static{ Scanner scan = new Scanner(System.in);
+        B = scan.nextInt(); scan.nextLine();
+        H = scan.nextInt(); scan.close();
+        if(B>0 && H>0){
+            flag = true;
+        }
+        else { flag=false; System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
 }
