6bf7f1ba06280891114b8f676aad540ea6e37167
David-B-Comer <davec2233@gmail.com>
2020-06-21 10:12:47 -0400

    added java anagrams solution

--- Java/JavaAnagrams/Solution.java
+++ Java/JavaAnagrams/Solution.java
@@ -73,5 +73,36 @@ package JavaAnagrams;
 
  **/
 
+import java.util.Scanner;
+
 public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        if(a.length() != b.length()) return false;
+
+        a = a.toUpperCase();
+        b = b.toUpperCase();
+
+        int c[] = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            c[a.charAt(i) - 'A']++;
+            c[b.charAt(i) - 'A']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (c[i] != 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
 }
