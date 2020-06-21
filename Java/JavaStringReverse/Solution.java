643e07fd84f3478e126de0461f83eb3435e991bc
David-B-Comer <davec2233@gmail.com>
2020-06-21 09:52:07 -0400

    added java string reverse problem

--- Java/JavaStringReverse/Solution.java
+++ Java/JavaStringReverse/Solution.java
@@ -1,4 +1,21 @@
 package JavaStringReverse;
 
+/**
+
+ A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.(Wikipedia)
+ Given a string , print Yes if it is a palindrome, print No otherwise.
+
+ Constraints
+
+ will consist at most  lower case english letters.
+ Sample Input
+
+ madam
+ Sample Output
+
+ Yes
+
+ **/
+
 public class Solution {
 }
