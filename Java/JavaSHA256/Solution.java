fd71a8790db1b00f76b5ae3abf54c4ccc5194d57
David-B-Comer <davec2233@gmail.com>
2020-06-20 20:33:08 -0400

    added java sha-256 solution

--- Java/JavaSHA256/Solution.java
+++ Java/JavaSHA256/Solution.java
@@ -34,5 +34,27 @@ package JavaSHA256;
 
  **/
 
+import java.security.MessageDigest;
+import java.util.Scanner;
+
 public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String message = in.nextLine();
+        in.close();
+
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(message.getBytes());
+            byte[] hash = messageDigest.digest();
+
+            for (byte b : hash) {
+                System.out.printf("%02x", b);
+            }
+        } catch (Exception e) {}
+    }
+
 }
+
