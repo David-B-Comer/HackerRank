91b51a5f9a3969f046c1f3def62b82b065b45a73
David-B-Comer <davec2233@gmail.com>
2020-06-28 17:07:19 -0400

    added java primality test solution

--- Java/JavaPrimalityTest/Solution.java
+++ Java/JavaPrimalityTest/Solution.java
@@ -29,5 +29,20 @@ package JavaPrimalityTest;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger n = in.nextBigInteger();
+        in.close();
+        System.out.println(n.isProbablePrime(1) ? "prime" :"not prime");
+    }
 }
