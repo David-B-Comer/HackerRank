d804ff4d05da1f35e38357c4118e1c62eb23e8e0
David-B-Comer <davec2233@gmail.com>
2020-06-29 19:44:02 -0400

    added java covariant return types solution

--- Java/CovariantReturnTypes/Solution.java
+++ Java/CovariantReturnTypes/Solution.java
@@ -37,5 +37,60 @@ package CovariantReturnTypes;
  An AndhraPradesh object's yourNationalFlower method returns an instance of the Lily class, and the Lily class' whatsYourName method returns Lily, which is printed by the hidden code checker.
 
  **/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Flower {
+    String whatsYourName() {
+        return "I have many names and types.";
+    }
+}
+class Jasmine extends Flower {
+    @Override
+    String whatsYourName() {
+        return "Jasmine";
+    }
+}
+class Lily extends Flower {
+    @Override
+    String whatsYourName() {
+        return "Lily";
+    }
+}
+class Region {
+    Flower yourNationalFlower() {
+        return new Flower();
+    }
+}
+class WestBengal extends Region {
+    @Override
+    Jasmine yourNationalFlower() {
+        return new Jasmine();
+    }
+}
+class AndhraPradesh extends Region {
+    @Override
+    Lily yourNationalFlower() {
+        return new Lily();
+    }
+}
+
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine().trim();
+        Region region = null;
+        switch (s) {
+            case "WestBengal":
+                region = new WestBengal();
+                break;
+            case "AndhraPradesh":
+                region = new AndhraPradesh();
+                break;
+        }
+        Flower flower = region.yourNationalFlower();
+        System.out.println(flower.whatsYourName());
+    }
 }
