4da604db769f8a350a6750774ffb4f655570c9a3
David-B-Comer <davec2233@gmail.com>
2020-07-05 20:47:43 -0400

    added java big integer solution

--- Java/JavaBigInteger/Solution.java
+++ Java/JavaBigInteger/Solution.java
@@ -30,5 +30,19 @@ package JavaBigInteger;
 
  **/
 
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
 public class Solution {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        BigInteger first = new BigInteger(scan.next());
+        BigInteger second = new BigInteger(scan.next());
+
+        System.out.println(first.add(second));
+        System.out.println(first.multiply(second));
+
+    }
 }
