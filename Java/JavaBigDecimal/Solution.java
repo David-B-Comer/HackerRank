e394f03e8cf62ed903b447e3a754a115846745c0
David-B-Comer <davec2233@gmail.com>
2020-07-05 20:25:06 -0400

    added java big decimal solution

--- Java/JavaBigDecimal/Solution.java
+++ Java/JavaBigDecimal/Solution.java
@@ -46,5 +46,36 @@ package JavaBigDecimal;
 
  **/
 
-public class Solution {
+
+import java.math.BigDecimal;
+import java.util.*;
+
+class Solution{
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+
+        s[n] = "";
+        s[n+1] = "";
+        Arrays.sort(s,((String number1,String number2) ->  {
+            if (!(number1.equals("")) && !(number2.equals(""))){
+                BigDecimal value1 = new BigDecimal(number1);
+                BigDecimal value2 = new BigDecimal(number2);
+                return value2.compareTo(value1);
+            }
+            return 0;})
+        );
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
 }
