3f1e548e7895f1fb105598c24c6fdc8f60949e96
David-B-Comer <davec2233@gmail.com>
2020-07-26 11:54:04 -0400

    added time conversion solution

--- Algorithms/TimeConversion/Solution.java
+++ Algorithms/TimeConversion/Solution.java
@@ -33,5 +33,41 @@ package TimeConversion;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static String timeConversion(String s){
+
+        int hours   = Integer.parseInt(s.substring(0,2));
+        int minutes = Integer.parseInt(s.substring(3,5));
+        int seconds = Integer.parseInt(s.substring(6,8));
+
+        if (s.substring(8, 10).equals("PM") && hours != 12){
+            hours += 12;
+        } else if (s.substring(8, 10).equals("AM") && hours == 12){
+            hours = 0;
+        }
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scan.nextLine();
+
+        String result = timeConversion(s);
+
+        bw.write(result);
+        bw.newLine();
+
+        bw.close();
+    }
 }
+
