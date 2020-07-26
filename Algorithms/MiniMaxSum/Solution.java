c6e91f24ebedaf2d13009b5abf180f5926f11d08
David-B-Comer <davec2233@gmail.com>
2020-07-26 11:33:52 -0400

    added mini max sum solution

--- Algorithms/MiniMaxSum/Solution.java
+++ Algorithms/MiniMaxSum/Solution.java
@@ -51,5 +51,50 @@ package MiniMaxSum;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static void miniMaxSum(int[] arr) {
+
+        long min = arr[0];
+        long max = arr[0];
+        long sum = arr[0];
+
+        for (int i = 1; i < arr.length; i++){
+            sum += arr[i];
+            if (arr[i] < min){
+                min = arr[i];
+            }
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.print((sum - max) + " " + (sum - min));
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
 }
