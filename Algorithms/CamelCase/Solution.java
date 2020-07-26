08412a39d85b32c5404d8087325b14027708bddf
David-B-Comer <davec2233@gmail.com>
2020-07-26 12:03:53 -0400

    added camel case solution

--- Algorithms/CamelCase/Solution.java
+++ Algorithms/CamelCase/Solution.java
@@ -49,5 +49,45 @@ package CamelCase;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static int camelcase(String s){
+
+        int words = 1;
+
+        for (int i = 0; i < s.length(); i++){
+            String letter = Character.toString(s.charAt(i));
+            if (letter == letter.toUpperCase()){
+                words++;
+            }
+        }
+        return words;
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        int result = camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
