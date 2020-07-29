8de121fb97f8ab40aaf27bcec9ad4bc295635230
David-B-Comer <davec2233@gmail.com>
2020-07-28 22:19:40 -0400

    added hacker rank in a string solution

--- Algorithms/HackerRankinaString/Solution.java
+++ Algorithms/HackerRankinaString/Solution.java
@@ -54,5 +54,52 @@ package HackerRankinaString;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static String hackerrankInString(String s){
+
+        String hackerrank = "hackerrank";
+        int index = 0;
+
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == hackerrank.charAt(index)){
+                index++;
+            }
+            if (index == hackerrank.length()){
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            String result = hackerrankInString(s);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
