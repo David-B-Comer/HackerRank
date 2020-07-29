2d1bbce53f53eb07f8ca728fb8684f146fe2f952
David-B-Comer <davec2233@gmail.com>
2020-07-28 22:39:43 -0400

    added pangrams solution

--- Algorithms/Pangrams/Solution.java
+++ Algorithms/Pangrams/Solution.java
@@ -54,5 +54,43 @@ package Pangrams;
 
  **/
 
- public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static String pangrams(String s){
+
+        s=s.toLowerCase();
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+
+        for(char c : alpha.toCharArray()){
+            if(!s.contains(""+c))
+                return "not pangram";
+        }
+        return "pangram";
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        String result = pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
