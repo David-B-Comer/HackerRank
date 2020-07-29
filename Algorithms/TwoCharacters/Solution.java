05c3ad81bfcaa7cd26529d36f6060e9099a45a47
David-B-Comer <davec2233@gmail.com>
2020-07-28 20:48:47 -0400

    added two characters solution

--- Algorithms/TwoCharacters/Solution.java
+++ Algorithms/TwoCharacters/Solution.java
@@ -47,5 +47,80 @@ package TwoCharacters;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 public class Solution {
+
+    public static final int NUM_LETTERS = 26;
+
+    static int alternate(String s){
+
+        int [][] pair  = new int[NUM_LETTERS][NUM_LETTERS];
+        int [][] count = new int[NUM_LETTERS][NUM_LETTERS];
+
+        if (s.length() <= 1){
+            return 0;
+        }
+
+        for (int i = 0; i < s.length(); i++){
+            char letter   = s.charAt(i);
+            int letterNum = letter - 'a';
+
+            for (int col = 0; col < NUM_LETTERS; col++){
+                if (pair[letterNum][col] == letter){
+                    count[letterNum][col] = -1;
+                }
+                if (count[letterNum][col] != -1){
+                    pair[letterNum][col] = letter;
+                    count[letterNum][col]++;
+                }
+            }
+
+            for (int row = 0; row < NUM_LETTERS; row++){
+                if (pair[row][letterNum] == letter){
+                    count[row][letterNum] = -1;
+                }
+                if (count[row][letterNum] != -1){
+                    pair[row][letterNum] = letter;
+                    count[row][letterNum]++;
+                }
+            }
+        }
+
+        int max = 0;
+        for (int row = 0; row < NUM_LETTERS; row++){
+            for (int col = 0; col < NUM_LETTERS; col++){
+                max = Math.max(max, count[row][col]);
+            }
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int l = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String s = bufferedReader.readLine();
+
+        int result = alternate(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
