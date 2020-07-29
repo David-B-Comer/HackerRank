b594097c31bd95efeba1ae132dcbea29432dff35
David-B-Comer <davec2233@gmail.com>
2020-07-28 23:37:38 -0400

    added weighted uniform strings solution

--- Algorithms/WeightedUniformStrings/Solution.java
+++ Algorithms/WeightedUniformStrings/Solution.java
@@ -87,5 +87,69 @@ package WeightedUniformStrings;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static String[] weightedUniformStrings(String s, int[] queries){
+
+        HashSet<Integer> weights = new HashSet();
+        int i=0;
+        int j=0;
+        String [] results = new String[queries.length];
+
+        while(j<s.length()){
+            if(s.charAt(i) == s.charAt(j)){
+                weights.add(((int)s.charAt(i)-96)*(j-i+1));
+                j++;
+            }else i++;
+        }
+        for(i=0; i<queries.length; i++){
+            if(weights.contains(queries[i]))
+                results[i]="Yes";
+            else results[i]="No";
+        }
+        return results;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        int queriesCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] queries = new int[queriesCount];
+
+        for (int i = 0; i < queriesCount; i++) {
+            int queriesItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            queries[i] = queriesItem;
+        }
+
+        String[] result = weightedUniformStrings(s, queries);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(result[i]);
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
