2252aa078e722d3380142f7bec84d1ebd9e0654b
David-B-Comer <davec2233@gmail.com>
2020-07-30 12:54:51 -0400

    added gemstones solution

--- Algorithms/Gemstones/Solution.java
+++ Algorithms/Gemstones/Solution.java
@@ -45,5 +45,60 @@ package Gemstones;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static int gemstones(String[] arr){
+
+        String[] alpha ={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","x","z"};
+        int count1 =0;
+        int count2 =0;
+
+        for(int i=0;i<alpha.length;i++){
+            count1=0;
+
+            for(int j=0; j<arr.length; j++){
+                if(arr[j].contains(alpha[i])){
+                    count1++;
+                }
+                if(count1==arr.length){
+                    count2++;
+                }
+            }
+        }
+        return count2;
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String arrItem = scanner.nextLine();
+            arr[i] = arrItem;
+        }
+
+        int result = gemstones(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
