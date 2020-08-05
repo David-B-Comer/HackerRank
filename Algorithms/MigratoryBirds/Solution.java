48a72b6e332f474eb92937acb963218bc1211876
David-B-Comer <davec2233@gmail.com>
2020-08-04 21:04:54 -0400

    added migratory birds solution

--- Algorithms/MigratoryBirds/Solution.java
+++ Algorithms/MigratoryBirds/Solution.java
@@ -63,5 +63,56 @@ package MigratoryBirds;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 public class Solution {
+
+    static final int types = 5;
+
+    static int migratoryBirds(List<Integer> arr){
+
+        int[] count = new int[types + 1];
+
+        for (int number : arr){
+            count[number]++;
+        }
+        int most = 1;
+
+        for (int i = 0; i < count.length; i++){
+            if (count[i] > count[most]){
+                most = i;
+            }
+        }
+        return most;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = migratoryBirds(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
