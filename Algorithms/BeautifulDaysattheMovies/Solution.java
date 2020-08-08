eb663f11456b0907184432180c89ee03a79088e5
David-B-Comer <davec2233@gmail.com>
2020-08-08 14:26:51 -0400

    added beautiful days at the movies solution

--- Algorithms/BeautifulDaysattheMovies/Solution.java
+++ Algorithms/BeautifulDaysattheMovies/Solution.java
@@ -45,5 +45,58 @@ package BeautifulDaysattheMovies;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static int beautifulDays(int i, int j, int k){
+
+        int start = i;
+        int end = j;
+        int result = 0;
+        int count = 0;
+        StringBuilder sb= new StringBuilder();
+
+        for(i = start; start <= end; start++){
+            sb.append("" + start);
+            sb.reverse();
+            result = Math.abs(start - Integer.parseInt(sb.toString())) % k;
+            sb.setLength(0);
+            if(result == 0){
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] ijk = scanner.nextLine().split(" ");
+
+        int i = Integer.parseInt(ijk[0]);
+
+        int j = Integer.parseInt(ijk[1]);
+
+        int k = Integer.parseInt(ijk[2]);
+
+        int result = beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
