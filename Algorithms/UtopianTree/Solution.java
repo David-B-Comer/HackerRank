9b716fcc98a8f914eeed59c8a73c2d3128cf0694
David-B-Comer <davec2233@gmail.com>
2020-08-08 10:28:47 -0400

    added utopian tree solution

--- Algorithms/UtopianTree/Solution.java
+++ Algorithms/UtopianTree/Solution.java
@@ -58,5 +58,49 @@ package UtopianTree;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static int utopianTree(int n) {
+
+        int height = 0;
+
+        if(n == 0)
+            return 1;
+        else if (n % 2 == 1)
+            height = height + (2 * utopianTree(n - 1));
+        else
+            height = height + (1 + utopianTree(n - 1));
+        return height;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = utopianTree(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
