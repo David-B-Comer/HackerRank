0a4fa90b4d585bb319701a624e131dd610765f39
David-B-Comer <davec2233@gmail.com>
2020-08-12 10:27:05 -0400

    added append and delete solution

--- Algorithms/AppendandDelete/Solution.java
+++ Algorithms/AppendandDelete/Solution.java
@@ -70,5 +70,57 @@ package AppendandDelete;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static String appendAndDelete(String s, String t, int k){
+        if (canConvert(s, t, k))
+            return "Yes";
+        return "No";
+    }
+
+    private static boolean canConvert(String s, String t, int k){
+
+        if (s.length() + t.length() <= k){
+            return true;
+        }
+        int i = 0;
+        for (; i < s.length() && i < t.length(); i++){
+            if (s.charAt(i) != t.charAt(i)){
+                break;
+            }
+        }
+        int minimumOperations = (s.length() - i) + (t.length() - i);
+        return k >= minimumOperations && (k - minimumOperations) % 2 == 0;
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        String t = scanner.nextLine();
+
+        int k = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String result = appendAndDelete(s, t, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
