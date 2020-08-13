db6477e514f1aa715929c7c85ae3da1c91be6eb1
David-B-Comer <davec2233@gmail.com>
2020-08-13 09:48:52 -0400

    added cut the sticks solution

--- Algorithms/CutTheSticks/Solution.java
+++ Algorithms/CutTheSticks/Solution.java
@@ -65,5 +65,68 @@ package CutTheSticks;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static int[] cutTheSticks(int[] arr){
+
+        Arrays.sort(arr);
+        List<Integer> list = new ArrayList<>();
+        list.add(arr.length);
+
+        for (int i = 1; i < arr.length; i++){
+            if(arr[i] != arr[i-1]){
+                list.add(arr.length - i);
+            }
+        }
+        int[] sticksCut = new int[list.size()];
+
+        for (int i = 0; i < sticksCut.length; i++){
+            sticksCut[i] = list.get(i);
+        }
+        return sticksCut;
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] result = cutTheSticks(arr);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
