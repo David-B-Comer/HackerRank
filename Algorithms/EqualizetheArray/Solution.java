681b2f6081fcd85ef21f8fedcf932ff456664d81
David-B-Comer <davec2233@gmail.com>
2020-08-15 19:11:35 -0400

    added equalize the array solution

--- Algorithms/EqualizetheArray/Solution.java
+++ Algorithms/EqualizetheArray/Solution.java
@@ -37,5 +37,67 @@ package EqualizetheArray;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static int equalizeArray(int[] arr){
+
+        int array[] = new int[101];
+
+        for (int i = 0; i < arr.length; i++){
+            array[arr[i]]++;
+        }
+        int max = getMax(array);
+
+        return arr.length - max;
+    }
+
+    private static int getMax(int[] array){
+
+        int max = Integer.MIN_VALUE;
+
+        for (int most : array){
+            if (most > max){
+                max = most;
+            }
+        }
+        return max;
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
