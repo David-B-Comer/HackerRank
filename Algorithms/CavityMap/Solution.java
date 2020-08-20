b3dcc2384e8ca8d11ad9a93668b4b2c8f350a8ae
David-B-Comer <davec2233@gmail.com>
2020-08-20 11:42:16 -0400

    added cavity map solution

--- Algorithms/CavityMap/Solution.java
+++ Algorithms/CavityMap/Solution.java
@@ -57,5 +57,68 @@ package CavityMap;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static String[] cavityMap(String[] grid){
+
+        char matrix[][] = new char[grid.length][grid.length];
+
+        for (int i = 0; i < grid.length; i++){
+            matrix[i] = grid[i].toCharArray();
+        }
+        for (int i = 1; i < grid.length - 1; i++){
+            for (int j = 1; j < grid.length - 1; j++){
+                if ((matrix[i - 1][j] < matrix[i][j]) &&
+                        (matrix[i + 1][j] < matrix[i][j]) &&
+                        (matrix[i][j - 1] < matrix[i][j]) &&
+                        (matrix[i][j + 1] < matrix[i][j]))
+                    matrix[i][j] = 'X';
+            }
+        }
+        for (int i = 0; i < grid.length; i++){
+            grid[i] = String.valueOf(matrix[i]);
+        }
+        return grid;
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] grid = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String gridItem = scanner.nextLine();
+            grid[i] = gridItem;
+        }
+
+        String[] result = cavityMap(grid);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(result[i]);
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
