c0e476a764da57945dddf2f1fcf16f530c45051c
David-B-Comer <davec2233@gmail.com>
2020-08-21 18:52:53 -0400

    manasa and stones solution

--- Algorithms/ManasaandStones/Solution.java
+++ Algorithms/ManasaandStones/Solution.java
@@ -69,5 +69,65 @@ package ManasaandStones;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static int[] stones(int n, int a, int b){
+
+        if (a == b){
+            return new int[]{(n - 1) * a};
+        }
+        int[] result = new int[n];
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
+
+        for (int i = 0; i < n; i++){
+            result[i] = (n - 1 - i) * min + i * max;
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int TItr = 0; TItr < T; TItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int a = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int b = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] result = stones(n, a, b);
+
+            for (int i = 0; i < result.length; i++) {
+                bufferedWriter.write(String.valueOf(result[i]));
+
+                if (i != result.length - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
