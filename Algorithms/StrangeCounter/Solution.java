9e97dcafb4a692549dbbc0666a9760c9c907566f
David-B-Comer <davec2233@gmail.com>
2020-08-23 17:16:22 -0400

    added strange counter solution

--- Algorithms/StrangeCounter/Solution.java
+++ Algorithms/StrangeCounter/Solution.java
@@ -42,5 +42,42 @@ package StrangeCounter;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static long strangeCounter(long t){
+
+        long value = 2;
+
+        while (3 * (value - 1) < t)
+            value = 2 * value;
+
+        return((3 * (value - 1) - t + 1));
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        long t = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = strangeCounter(t);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
