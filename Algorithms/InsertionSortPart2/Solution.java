e69660ea13e2518fa24bfce2e3b023d9f028e1ae
David-B-Comer <davec2233@gmail.com>
2020-08-25 13:44:52 -0400

    added insertion sort part 2 solution

--- Algorithms/InsertionSortPart2/Solution.java
+++ Algorithms/InsertionSortPart2/Solution.java
@@ -67,5 +67,57 @@ package InsertionSortPart2;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static void insertionSort2(int n, int[] arr){
+
+        for (int i = 1; i < arr.length; i++){
+            int j = i;
+            int number = arr[i];
+
+            while (j >= 1 && arr[j - 1] > number){
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = number;
+            print(arr);
+        }
+    }
+    private static void print(int[] arr){
+        for (int i: arr){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        insertionSort2(n, arr);
+
+        scanner.close();
+    }
 }
