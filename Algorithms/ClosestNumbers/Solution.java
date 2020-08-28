4566ba8971cef7835d88b4888f55b0e0dce84a85
David-B-Comer <davec2233@gmail.com>
2020-08-28 18:28:56 -0400

    added closest numbers solution

--- Algorithms/ClosestNumbers/Solution.java
+++ Algorithms/ClosestNumbers/Solution.java
@@ -59,5 +59,72 @@ package ClosestNumbers;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static int[] closestNumbers(int[] arr){
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(arr);
+        int difference = arr[arr.length - 1] - arr[0];
+
+        for (int i = 0; i < arr.length - 1; i++)
+            if ((Math.abs(arr[i + 1]) - arr[i]) < difference)
+                difference = Math.abs(arr[i + 1] - arr[i]);
+
+        for (int i = 0; i < arr.length - 1; i++)
+            if ((Math.abs(arr[i + 1] - arr[i])) == difference){
+                list.add(arr[i]);
+                list.add(arr[i + 1]);
+            }
+
+        int[] result = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++)
+            result[i] = list.get(i);
+
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] result = closestNumbers(arr);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
