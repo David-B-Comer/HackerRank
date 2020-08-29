60b02a1f2c8d70ac3f972d086c8978a5239511ac
David-B-Comer <davec2233@gmail.com>
2020-08-29 09:10:21 -0400

    added string construction solution

--- Algorithms/StringConstruction/Solution.java
+++ Algorithms/StringConstruction/Solution.java
@@ -63,5 +63,53 @@ package StringConstruction;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static int stringConstruction(String s){
+
+        int[] array = new int[26];
+
+        for (int i = 0; i < s.length(); i++){
+            char chars = s.charAt(i);
+            array[chars - 97]++;
+        }
+        int minimumCost = 0;
+
+        for (int i = 0; i < array.length; i++){
+            if (array[i] != 0)
+                minimumCost++;
+        }
+        return minimumCost;
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = stringConstruction(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
