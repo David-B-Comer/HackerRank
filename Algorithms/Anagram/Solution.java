05038db1cd43c16f872ef82078bd1e7924d9b792
David-B-Comer <davec2233@gmail.com>
2020-08-30 08:02:32 -0400

    added anagram solution

--- Algorithms/Anagram/Solution.java
+++ Algorithms/Anagram/Solution.java
@@ -62,5 +62,54 @@ package Anagram;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static int anagram(String s){
+
+        if (s.length() % 2 != 0)
+            return -1;
+
+        StringBuilder word1 = new StringBuilder(s.substring(0, s.length() / 2));
+        StringBuilder word2 = new StringBuilder(s.substring(s.length() / 2));
+
+        for (char c : word2.toString().toCharArray()){
+            int i = word1.indexOf(String.valueOf(c));
+
+            if (i > -1)
+                word1 = word1.delete(i, i + 1);
+        }
+        return word1.length();
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = anagram(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
