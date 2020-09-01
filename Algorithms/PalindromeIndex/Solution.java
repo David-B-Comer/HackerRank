adaf9fe89f2bf27f9e1fe5d4a5125e9cc19ec739
David-B-Comer <davec2233@gmail.com>
2020-09-01 09:21:40 -0400

    added palindrome index solution

--- Algorithms/PalindromeIndex/Solution.java
+++ Algorithms/PalindromeIndex/Solution.java
@@ -49,5 +49,65 @@ package PalindromeIndex;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static int palindromeIndex(String s){
+
+        int j = s.length() - 1;
+
+        if (palindrome(s))
+            return - 1;
+        for (int i = 0; i < j; i++, j--){
+            if (s.charAt(i) != s.charAt(j)){
+                if (palindrome(s.substring(i, j)))
+                    return j;
+                else if (palindrome(s.substring(i + 1, j + 1)))
+                    return i;
+            }
+        }
+        return -1;
+    }
+
+    static boolean palindrome(String s){
+
+        int j = s.length() - 1;
+
+        for (int i = 0; i < s.length() - 1; i++, j--){
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+        }
+        return true;
+    }
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = palindromeIndex(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
