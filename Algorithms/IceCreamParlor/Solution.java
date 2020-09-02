dbc19cb6c120ed34f62cc5740a28d492a05036da
David-B-Comer <davec2233@gmail.com>
2020-09-02 09:34:31 -0400

    added ice cream parlor solution

--- Algorithms/IceCreamParlor/Solution.java
+++ Algorithms/IceCreamParlor/Solution.java
@@ -55,5 +55,77 @@ package IceCreamParlor;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static int[] icecreamParlor(int m, int[] arr){
+
+        int[] flavorPair = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++){
+            int flavor1Cost = arr[i];
+            int flavor2Cost = m - flavor1Cost;
+            Integer flavor2Index = map.get(flavor2Cost);
+
+            if (flavor2Index != null){
+                flavorPair[0] = flavor2Index + 1;
+                flavorPair[1] = i + 1;
+                break;
+            }
+            map.put(flavor1Cost, i);
+        }
+        return flavorPair;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int m = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] arr = new int[n];
+
+            String[] arrItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrItems[i]);
+                arr[i] = arrItem;
+            }
+
+            int[] result = icecreamParlor(m, arr);
+
+            for (int i = 0; i < result.length; i++) {
+                bufferedWriter.write(String.valueOf(result[i]));
+
+                if (i != result.length - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
