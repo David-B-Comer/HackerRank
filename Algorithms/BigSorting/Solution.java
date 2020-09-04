324239a170fc61bb43b9c0b4c7aa024dda2f8135
David-B-Comer <davec2233@gmail.com>
2020-09-03 20:00:20 -0400

    added big sorting solution

--- Algorithms/BigSorting/Solution.java
+++ Algorithms/BigSorting/Solution.java
@@ -71,5 +71,60 @@ package BigSorting;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static String[] bigSorting(String[] unsorted){
+
+        BigInteger[] unsortedArray = new BigInteger[unsorted.length];
+
+        for (int i = 0; i < unsorted.length; i++)
+            unsortedArray[i] = new BigInteger(unsorted[i]);
+        Arrays.sort(unsortedArray);
+        String[] sorted = new String[unsorted.length];
+
+        for (int i = 0; i < unsorted.length; i++)
+            sorted[i] = unsortedArray[i].toString();
+
+        return sorted;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] unsorted = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String unsortedItem = scanner.next();
+            unsorted[i] = unsortedItem;
+        }
+
+        String[] result = bigSorting(unsorted);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(result[i]);
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
