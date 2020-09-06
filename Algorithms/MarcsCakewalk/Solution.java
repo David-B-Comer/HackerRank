8254b1e935607e3c53ef1cc0fe5e77aa95d2d6d6
David-B-Comer <davec2233@gmail.com>
2020-09-06 19:22:55 -0400

    added marcs cakewalk solution

--- Algorithms/MarcsCakewalk/Solution.java
+++ Algorithms/MarcsCakewalk/Solution.java
@@ -53,5 +53,57 @@ package MarcsCakewalk;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static long marcsCakewalk(int[] calorie){
+
+        Arrays.sort(calorie);
+        int j = 0;
+        long miles = 0;
+
+        for (int i = (calorie.length - 1); i >= 0; i--){
+            miles = miles + (long)(calorie[i] * Math.pow(2, j));
+            j++;
+        }
+        return miles;
+    }
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] calorie = new int[n];
+
+        String[] calorieItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int calorieItem = Integer.parseInt(calorieItems[i]);
+            calorie[i] = calorieItem;
+        }
+
+        long result = marcsCakewalk(calorie);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
