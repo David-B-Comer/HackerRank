31039dd5d278ee1607356064972ba5b5f1162741
David-B-Comer <davec2233@gmail.com>
2020-09-06 19:48:55 -0400

    added grid challenge solution

--- Algorithms/GridChallenge/Solution.java
+++ Algorithms/GridChallenge/Solution.java
@@ -61,5 +61,60 @@ package GridChallenge;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static String gridChallenge(String[] grid){
+
+        for (int i = 0; i < grid.length; i++){
+            char[] chars = grid[i].toCharArray();
+            Arrays.sort(chars);
+            grid[i] = new String(chars);
+        }
+        for (int i = 1; i < grid.length; i++){
+            for (int j = 0; j < grid[i].length(); j++){
+                if (grid[i - 1].charAt(j) > grid[i].charAt(j))
+                    return "NO";
+            }
+        }
+        return "YES";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            String[] grid = new String[n];
+
+            for (int i = 0; i < n; i++) {
+                String gridItem = scanner.nextLine();
+                grid[i] = gridItem;
+            }
+
+            String result = gridChallenge(grid);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
