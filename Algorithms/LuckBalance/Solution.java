79732f5808aefccc14f29da5f8ebc3433c019801
David-B-Comer <davec2233@gmail.com>
2020-09-07 09:51:19 -0400

    added luck balance solution

--- Algorithms/LuckBalance/Solution.java
+++ Algorithms/LuckBalance/Solution.java
@@ -53,5 +53,70 @@ package LuckBalance;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static int luckBalance(int k, int[][] contests){
+
+        int maximumLuck = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < contests.length; i++){
+            maximumLuck += contests[i][0];
+            if (contests[i][1] == 1){
+                list.add(contests[i][0]);
+            }
+        }
+
+        Collections.sort(list);
+        int lostImportant = list.size() - k - 1;
+
+        while (lostImportant > -1){
+            maximumLuck -= 2 * list.get(lostImportant);
+            lostImportant--;
+        }
+        return maximumLuck;
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[][] contests = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            String[] contestsRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 2; j++) {
+                int contestsItem = Integer.parseInt(contestsRowItems[j]);
+                contests[i][j] = contestsItem;
+            }
+        }
+
+        int result = luckBalance(k, contests);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
