78b14597b099db42e8275c6a3c8cd1c41484a5c8
David-B-Comer <davec2233@gmail.com>
2020-09-13 12:54:09 -0400

    added mark and toys solution

--- Algorithms/MarkandToys/Solution.java
+++ Algorithms/MarkandToys/Solution.java
@@ -43,5 +43,60 @@ package MarkandToys;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static int maximumToys(int[] prices, int k){
+
+        int toys = 0;
+        int moneySpent = 0;
+        Arrays.sort(prices);
+
+        for (int i = 0; i < prices.length; i++){
+            if (moneySpent + prices[i] <= k){
+                moneySpent = moneySpent + prices[i];
+                toys++;
+            }
+        }
+        return toys;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] prices = new int[n];
+
+        String[] pricesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int pricesItem = Integer.parseInt(pricesItems[i]);
+            prices[i] = pricesItem;
+        }
+
+        int result = maximumToys(prices, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
