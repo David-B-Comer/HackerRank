c1f65289caf0621d50808cd4668beab7ba94609d
David-B-Comer <davec2233@gmail.com>
2020-09-24 18:06:53 -0400

    added introduction to nim game solution

--- Algorithms/IntroductionToNimGame/Solution.java
+++ Algorithms/IntroductionToNimGame/Solution.java
@@ -61,5 +61,61 @@ package IntroductionToNimGame;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static String nimGame(int[] pile){
+
+        int result = 0;
+
+        for (int i = 0; i < pile.length; i++)
+            result ^= pile[i];
+
+        if (result == 0)
+            return "Second";
+        else
+            return "First";
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int g = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int gItr = 0; gItr < g; gItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] pile = new int[n];
+
+            String[] pileItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int pileItem = Integer.parseInt(pileItems[i]);
+                pile[i] = pileItem;
+            }
+
+            String result = nimGame(pile);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
