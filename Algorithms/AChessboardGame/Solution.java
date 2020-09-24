0dd8d997f3b0b55b9a61ef051478691efe5b2c46
David-B-Comer <davec2233@gmail.com>
2020-09-23 20:46:55 -0400

    added a chessboard game solution

--- Algorithms/AChessboardGame/Solution.java
+++ Algorithms/AChessboardGame/Solution.java
@@ -60,5 +60,48 @@ package AChessboardGame;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static String chessboardGame(int x, int y){
+
+        if ((x % 4 == 1 || x % 4 == 2) && (y % 4 == 1 || y % 4 == 2)){
+            return("Second");
+        }
+        return("First");
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] xy = scanner.nextLine().split(" ");
+
+            int x = Integer.parseInt(xy[0]);
+
+            int y = Integer.parseInt(xy[1]);
+
+            String result = chessboardGame(x, y);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
