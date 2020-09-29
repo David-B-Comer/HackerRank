ec6dd7268cff9c47e1441220e2c19c9bcccfd861
David-B-Comer <davec2233@gmail.com>
2020-09-29 08:47:57 -0400

    added encryption solution

--- Algorithms/Encryption/Solution.java
+++ Algorithms/Encryption/Solution.java
@@ -87,5 +87,44 @@ An English text needs to be encrypted using the following encryption scheme.
         ut
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static String encryption(String s){
+
+        int columns = (int)Math.ceil(Math.sqrt(Double.valueOf(s.length())));
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < columns; i++){
+            for (int j = i; j < s.length(); j += columns){
+                sb.append(s.charAt(j));
+            }
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        String result = encryption(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
