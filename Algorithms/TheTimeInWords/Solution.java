45d4285f430061e2f86216e5b45522535e2f5e18
David-B-Comer <davec2233@gmail.com>
2020-09-30 08:52:44 -0400

    added the time in words solution

--- Algorithms/TheTimeInWords/Solution.java
+++ Algorithms/TheTimeInWords/Solution.java
@@ -48,5 +48,69 @@ package TheTimeInWords;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static String timeInWords(int h, int m){
+
+        String numbers[] = { "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine",
+                "ten", "eleven", "twelve", "thirteen",
+                "fourteen", "fifteen", "sixteen", "seventeen",
+                "eighteen", "nineteen", "twenty", "twenty one",
+                "twenty two", "twenty three", "twenty four",
+                "twenty five", "twenty six", "twenty seven",
+                "twenty eight", "twenty nine",
+        };
+        String time = "";
+
+        if (m == 0)
+            time = (numbers[h] + " o' clock ");
+        else if (m == 1)
+            time = ("one minute past " + numbers[h]);
+        else if (m == 59)
+            time = ("one minute to " + numbers[(h % 12) + 1]);
+        else if (m == 15)
+            time = ("quarter past " + numbers[h]);
+        else if (m == 30)
+            time = ("half past " + numbers[h]);
+        else if (m == 45)
+            time = ("quarter to " + numbers[(h % 12) + 1]);
+        else if (m <= 30)
+            time = (numbers[m] + " minutes past " + numbers[h]);
+        else if (m > 30)
+            time = (numbers[60 - m] + " minutes to " + numbers[(h % 12) + 1]);
+
+        return time;
+    }
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int h = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int m = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String result = timeInWords(h, m);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
