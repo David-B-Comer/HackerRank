a1a9015f02703f9bd04d59da9d5b1c0022a82c30
David-B-Comer <davec2233@gmail.com>
2020-10-01 08:25:58 -0400

    added 3d surface area solution

--- Algorithms/M3DSurfaceArea/Solution.java
+++ Algorithms/M3DSurfaceArea/Solution.java
@@ -46,5 +46,77 @@ package M3DSurfaceArea;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static int surfaceArea(int[][] A){
+
+        int area = 0;
+
+        for (int i = 0; i < A.length; i++){
+            int rowArea = A[i][0];
+
+            for (int j = 0; j < A[i].length - 1; j++){
+                if (A[i][j + 1] > A[i][j]){
+                    rowArea += A[i][j + 1] - A[i][j];
+                }
+            }
+            area += rowArea * 2;
+        }
+
+        for (int i = 0; i < A[0].length; i++){
+            int columnArea = A[0][i];
+
+            for (int j = 0; j < A.length - 1; j++){
+                if (A[j + 1][i] > A[j][i]){
+                    columnArea += A[j + 1][i] - A[j][i];
+                }
+            }
+            area += columnArea * 2;
+        }
+        area += A.length * A[0].length * 2;
+
+        return area;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] HW = scanner.nextLine().split(" ");
+
+        int H = Integer.parseInt(HW[0]);
+
+        int W = Integer.parseInt(HW[1]);
+
+        int[][] A = new int[H][W];
+
+        for (int i = 0; i < H; i++) {
+            String[] ARowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < W; j++) {
+                int AItem = Integer.parseInt(ARowItems[j]);
+                A[i][j] = AItem;
+            }
+        }
+
+        int result = surfaceArea(A);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
