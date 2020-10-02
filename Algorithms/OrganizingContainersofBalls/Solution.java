081cc973de451befbc81f8683f7b7336453e7d8a
David-B-Comer <davec2233@gmail.com>
2020-10-02 08:44:03 -0400

    added organizing containers of balls solution

--- Algorithms/OrganizingContainersofBalls/Solution.java
+++ Algorithms/OrganizingContainersofBalls/Solution.java
@@ -84,5 +84,71 @@ package OrganizingContainersofBalls;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static String organizingContainers(int[][] container){
+
+        int length = container.length;
+        int[] container_0 = new int[length];
+        int[] container_1 = new int[length];
+
+        for (int i = 0; i < length; i++){
+            for (int j = 0; j < length; j++){
+                container_0[i] += container[i][j];
+                container_1[j] += container[i][j];
+            }
+        }
+        Arrays.sort(container_0);
+        Arrays.sort(container_1);
+
+        for (int i = 0; i < length; i++){
+            if (container_0[i] != container_1[i]){
+                return "Impossible";
+            }
+        }
+        return "Possible";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[][] container = new int[n][n];
+
+            for (int i = 0; i < n; i++) {
+                String[] containerRowItems = scanner.nextLine().split(" ");
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                for (int j = 0; j < n; j++) {
+                    int containerItem = Integer.parseInt(containerRowItems[j]);
+                    container[i][j] = containerItem;
+                }
+            }
+
+            String result = organizingContainers(container);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
