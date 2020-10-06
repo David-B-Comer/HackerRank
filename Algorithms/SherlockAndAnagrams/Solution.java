24c18574bb1304e3f17149bf6d358e91294fab29
David-B-Comer <davec2233@gmail.com>
2020-10-06 18:41:46 -0400

    added sherlock and anagrams solution

--- Algorithms/SherlockAndAnagrams/Solution.java
+++ Algorithms/SherlockAndAnagrams/Solution.java
@@ -75,5 +75,64 @@ package SherlockAndAnagrams;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static int sherlockAndAnagrams(String s){
+        int anagramPairs = 0;
+
+        for (int i = 0; i < s.length() - 1; i++){
+            for (int j = 0; j < s.length() - i - 1; j++){
+                String substr = s.substring(j, j + i + 1);
+                for (int k = j + 1; k < s.length() - substr.length() + 1; k++){
+                    if (anagram(substr, s.substring(k, k + substr.length()))){
+                        anagramPairs++;
+                    }
+                }
+            }
+        }
+        return anagramPairs;
+    }
+
+    static boolean anagram(String string1, String string2){
+
+        if (string1.length() != string2.length())
+            return false;
+        char[] substr1 = string1.toCharArray();
+        char[] substr2 = string2.toCharArray();
+        Arrays.sort(substr1);
+        Arrays.sort(substr2);
+
+        return Arrays.equals(substr1, substr2);
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = sherlockAndAnagrams(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
