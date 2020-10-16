c22d88f7d92d516a2f88d824748ac926ab880cd6
David-B-Comer <davec2233@gmail.com>
2020-10-16 14:05:09 -0400

    added new year chaos solution

--- InterviewPreparationKit/NewYearChaos/Solution.java
+++ InterviewPreparationKit/NewYearChaos/Solution.java
@@ -73,5 +73,60 @@ package NewYearChaos;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static void minimumBribes(int[] q){
+        int bribes = 0;
+        boolean tooChaotic = false;
+
+        for (int i = 0; i < q.length; i++){
+            if (q[i] - (i + 1) > 2){
+                tooChaotic = true;
+                break;
+            }
+            for (int j = Math.max(0, q[i] - 2); j < i; j++){
+                if (q[j] > q[i])
+                    bribes++;
+            }
+        }
+        if (tooChaotic)
+            System.out.println("Too chaotic");
+        else
+            System.out.println(bribes);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] q = new int[n];
+
+            String[] qItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int qItem = Integer.parseInt(qItems[i]);
+                q[i] = qItem;
+            }
+
+            minimumBribes(q);
+        }
+
+        scanner.close();
+    }
 }
+
