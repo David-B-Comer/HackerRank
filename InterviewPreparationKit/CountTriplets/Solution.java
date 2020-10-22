1605f6532563cdafdddab6583dd7c314bdd4898a
David-B-Comer <davec2233@gmail.com>
2020-10-22 19:43:38 -0400

    added count triplets solution

--- InterviewPreparationKit/CountTriplets/Solution.java
+++ InterviewPreparationKit/CountTriplets/Solution.java
@@ -60,5 +60,59 @@ package CountTriplets;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution{
+
+    static long countTriplets(List<Long> arr, long r){
+        long triplets = 0;
+        HashMap<Long, Long> map = new HashMap<>();
+        HashMap<Long, Long> map2 = new HashMap<>();
+
+        for (Long n : arr){
+            if (n % r == 0){
+                if (map2.containsKey(n / r)){
+                    triplets += map2.get(n / r);
+                }
+                if (map.containsKey(n / r)){
+                    map2.put(n, map2.getOrDefault(n, 0L) + map.get(n / r));
+                }
+            }
+            map.put(n, map.getOrDefault(n, 0L) + 1);
+        }
+        return triplets;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nr[0]);
+
+        long r = Long.parseLong(nr[1]);
+
+        List<Long> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long ans = countTriplets(arr, r);
+
+        bufferedWriter.write(String.valueOf(ans));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
