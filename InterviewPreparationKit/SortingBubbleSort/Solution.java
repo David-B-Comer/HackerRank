0aadea2a73c8b8b4383ac0cc30fc3bef9108814d
David-B-Comer <davec2233@gmail.com>
2020-10-26 16:36:00 -0400

    added sorting bubble sort solution

--- InterviewPreparationKit/SortingBubbleSort/Solution.java
+++ InterviewPreparationKit/SortingBubbleSort/Solution.java
@@ -82,5 +82,60 @@ package SortingBubbleSort;
 
  **/
 
-public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static void countSwaps(int[] a){
+        int numSwaps = 0;
+        int firstElement;
+        int lastElement;
+        int n = a.length;
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n - 1; j++){
+                if (a[j] > a[j + 1]){
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    numSwaps++;
+                }
+            }
+        }
+        firstElement = a[0];
+        lastElement = a[n - 1];
+
+        System.out.println("Array is sorted in " + numSwaps + " swaps.");
+        System.out.println("First Element: " + firstElement);
+        System.out.println("Last Element: " + lastElement);
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        countSwaps(a);
+
+        scanner.close();
+    }
 }
+
