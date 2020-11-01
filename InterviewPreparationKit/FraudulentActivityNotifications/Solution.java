f28fa0dddf65b42dfbc05e814b852c1bf7c80a30
David-B-Comer <davec2233@gmail.com>
2020-11-01 10:46:44 -0500

    added fraudulent activity notifications solution not meeting time limits

--- InterviewPreparationKit/FraudulentActivityNotifications/Solution.java
+++ InterviewPreparationKit/FraudulentActivityNotifications/Solution.java
@@ -59,5 +59,70 @@ package FraudulentActivityNotifications;
 
  **/
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    static int activityNotifications(int[] expenditure, int d){
+        int[] spendingDays = new int[d];
+        int notification = 0;
+
+        for (int i = 0; i < expenditure.length - d - 1; i++){
+            for (int j = 0; j < d; j++){
+                spendingDays[j] = expenditure[i + j];
+            }
+            Arrays.sort(spendingDays);
+
+            if (d % 2 == 0){
+                int median = spendingDays[d / 2 + 1] + spendingDays[d / 2 + 2];
+
+                if (median * 2 >= expenditure[d + i + 1])
+                    notification++;
+            }
+            else{
+                int median = spendingDays[(d + 1) / 2];
+                if (median * 2 >= expenditure[d + i + 1])
+                    notification++;
+            }
+        }
+        return notification;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] expenditure = new int[n];
+
+        String[] expenditureItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int expenditureItem = Integer.parseInt(expenditureItems[i]);
+            expenditure[i] = expenditureItem;
+        }
+
+        int result = activityNotifications(expenditure, d);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
+
