deb00491879ff894fd46e294dfadb654af1754b1
David-B-Comer <davec2233@gmail.com>
2020-11-04 09:26:04 -0500

    added strings making anagrams solution

--- InterviewPreparationKit/StringsMakingAnagrams/Solution.java
+++ InterviewPreparationKit/StringsMakingAnagrams/Solution.java
@@ -47,5 +47,52 @@ package StringsMakingAnagrams;
 
  **/
 
-public class Solution {
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+
+    static int makeAnagram(String a, String b){
+
+        int count1[] = new int[26];
+        int count2[] = new int[26];
+
+        for (int i = 0; i < a.length(); i++)
+            count1[a.charAt(i) - 'a']++;
+
+        for (int i = 0; i < b.length(); i++)
+            count2[b.charAt(i) - 'a']++;
+
+        int deletions = 0;
+
+        for (int i = 0; i < 26; i++)
+            deletions += Math.abs(count1[i] - count2[i]);
+
+        return deletions;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String a = scanner.nextLine();
+
+        String b = scanner.nextLine();
+
+        int res = makeAnagram(a, b);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
